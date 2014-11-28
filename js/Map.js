a7e011ad01caac8af951e9a5ff23bb0fc6866c24
eluxtechnologies14 <msgarsal@gmail.com>
2014-11-28 16:15:29 -0600

    Create Map.js

---
+++ js/Map.js
@@ -0,0 +1,21 @@
+var origin1 = new google.maps.LatLng(41.8369, 87.6847);
+var origin2 = "Chicago, Illinois";
+var destinationA = "Oak Park, Illinois";
+var destinationB = new google.maps.LatLng(41.8833, 87.8000);
+
+var service = new google.maps.DistanceMatrixService();
+service.getDistanceMatrix(
+  {
+    origins: [origin1, origin2],
+    destinations: [destinationA, destinationB],
+    travelMode: google.maps.TravelMode.DRIVING,
+    unitSystem: UnitSystem,
+    durationInTraffic: Boolean,
+    avoidHighways: false,
+    avoidTolls: false
+  }, callback);
+
+function callback(response, status) {
+  // See Parsing the Results for
+  // the basics of a callback function.
+}
